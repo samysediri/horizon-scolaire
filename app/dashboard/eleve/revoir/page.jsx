dae25c1908fa4fcb2b7b69545fda2ea60c834db0
samysediri <samysediri65@gmail.com>
2025-04-01 17:30:26 -0400

    Add files via upload

---
+++ app/dashboard/eleve/revoir/page.jsx
@@ -0,0 +1,10 @@
+'use client'
+
+export default function RevoirSeances() {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">Revoir mes séances</h1>
+      <p>Accédez ici aux enregistrements des séances passées.</p>
+    </div>
+  )
+}
