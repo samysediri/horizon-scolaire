11867260097d663d42f9155bf96923125eb343b4
samysediri <samysediri65@gmail.com>
2025-04-02 19:09:20 -0400

    Add files via upload

---
+++ app/auth/callback/page.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useEffect } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { createClient } from '@/utils/supabase/client.ts';
+;
+
+export default function CallbackPage() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const code = searchParams.get('code');
+
+  useEffect(() => {
+    const supabase = createClient();
+
+    async function handleSignIn() {
+      const { error } = await supabase.auth.exchangeCodeForSession(code);
+      if (error) {
+        console.error('Erreur de session:', error);
+        return;
+      }
+      router.push('/dashboard');
+    }
+
+    if (code) handleSignIn();
+  }, [code, router]);
+
+  return <p>Connexion en cours...</p>;
+}
