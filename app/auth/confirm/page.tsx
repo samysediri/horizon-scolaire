6b822b41c0398e46aa4ef0109535dbd66c8e168c
samysediri <samysediri65@gmail.com>
2025-04-03 17:30:09 -0400

    Update page.tsx

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import ConfirmClient from './ConfirmClient'
 
 export default function Page() {
-  return <ConfirmClient />
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <ConfirmClient />
+    </Suspense>
+  )
 }
