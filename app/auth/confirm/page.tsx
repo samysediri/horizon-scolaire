1d1a282861eeff028f51ce7f913757f4b97d94da
samysediri <samysediri65@gmail.com>
2025-04-03 16:58:17 -0400

    Update page.tsx

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,31 +1,5 @@
-'use client'
+import ConfirmPage from './ConfirmClient'
 
-import { useSearchParams, useRouter } from 'next/navigation'
-import { useEffect } from 'react'
-import { createClient } from '@/utils/supabase/client'
-
-export default function ConfirmPage() {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-
-  useEffect(() => {
-    const confirm = async () => {
-      const code = searchParams.get('code')
-      const supabase = createClient()
-
-      if (code) {
-        const { error } = await supabase.auth.exchangeCodeForSession(code)
-
-        if (!error) {
-          router.push('/dashboard/tuteur')
-        } else {
-          console.error('Erreur de session Supabase :', error)
-        }
-      }
-    }
-
-    confirm()
-  }, [searchParams, router])
-
-  return <p>Confirmation en cours...</p>
+export default function Page() {
+  return <ConfirmPage />
 }
