1c7d416b38b7c747f2a027dd7de204dd43d8dd03
samysediri <samysediri65@gmail.com>
2025-04-03 17:20:29 -0400

    Update page.tsx

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -1,19 +1,5 @@
-'use client'
+import ConfirmClient from './ConfirmClient'
 
-import { useSearchParams } from 'next/navigation'
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
-
-export default function ConfirmPage() {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-
-  useEffect(() => {
-    const code = searchParams.get('code')
-    if (code) {
-      router.push('/dashboard/tuteur')
-    }
-  }, [searchParams, router])
-
-  return <div>Redirection en cours...</div>
+export default function Page() {
+  return <ConfirmClient />
 }
