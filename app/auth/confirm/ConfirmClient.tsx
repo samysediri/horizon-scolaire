65a9776ea2965d7d79375087ec510be9fb42b9f5
samysediri <samysediri65@gmail.com>
2025-04-03 16:53:35 -0400

    Create ConfirmClient.tsx

---
+++ app/auth/confirm/ConfirmClient.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { useSearchParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+import { createClient } from '@/utils/supabase/client'
+
+export default function ConfirmClient() {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  useEffect(() => {
+    const confirm = async () => {
+      const code = searchParams.get('code')
+      const supabase = createClient()
+
+      if (code) {
+        const { error } = await supabase.auth.exchangeCodeForSession(code)
+
+        if (!error) {
+          router.push('/dashboard/tuteur')
+        } else {
+          console.error('Erreur de session Supabase :', error)
+        }
+      }
+    }
+
+    confirm()
+  }, [searchParams, router])
+
+  return <p>Confirmation en cours...</p>
+}
