5e78a0632c78e698500b376baf65ce662e19347b
samysediri <samysediri65@gmail.com>
2025-04-04 17:21:02 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,7 +1,7 @@
-import { createBrowserClient as createClient } from '@supabase/ssr'
+import { createBrowserClient } from '@supabase/ssr'
 
-export const createBrowserClient = () =>
-  createClient(
+export const createClient = () =>
+  createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
