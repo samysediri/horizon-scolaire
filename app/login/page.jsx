eed74706aff4d5cf6804557d3be0c758879ca9e1
samysediri <samysediri65@gmail.com>
2025-04-05 13:26:05 -0400

    Update page.jsx

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { createClient } from '@/lib/supabase/client'
+import { createBrowserClient as createClient } from '@/lib/supabase/client'
+
 
 export default function LoginPage() {
   const [email, setEmail] = useState('')
