943ca04344d9007e7be7ca11a2a07e10168dfe42
samysediri <samysediri65@gmail.com>
2025-04-05 14:59:33 -0400

    Update page.tsx

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -10,21 +10,20 @@ export default function ConfirmPage() {
   useEffect(() => {
     const run = async () => {
       const supabase = createClient()
-      const { error } = await supabase.auth.exchangeCodeForSession(window.location.href)
+
+      const { error } = await supabase.auth.exchangeCodeForSession(
+        window.location.href
+      )
 
       if (error) {
         console.error('Erreur de session:', error.message)
       } else {
-        router.push('/dashboard') // ou ta page d’accueil après login
+        router.push('/')
       }
     }
 
     run()
   }, [router])
 
-  return (
-    <div className="flex justify-center items-center h-screen">
-      <p>Connexion en cours...</p>
-    </div>
-  )
+  return <p>Connexion en cours...</p>
 }
