03fbd7fba68493c69328514e443a7222f4fd188c
samysediri <samysediri65@gmail.com>
2025-04-05 19:28:21 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -2,15 +2,11 @@
 
 'use client'
 
-import { createBrowserClient } from '@supabase/ssr'
+import { createPagesBrowserClient } from '@supabase/ssr'
 
 export const createClient = () => {
-  if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
-    throw new Error('Les variables d’environnement Supabase sont manquantes.')
-  }
-
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  )
-}
+  return createPagesBrowserClient({
+    supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  });
+};
