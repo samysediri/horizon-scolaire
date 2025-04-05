59aec88c5cd0321569d10e1b67fd23c51b7e8d33
samysediri <samysediri65@gmail.com>
2025-04-05 16:32:25 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,8 +1,15 @@
+// ./lib/supabase/client.ts
+
 import { createBrowserClient } from '@supabase/ssr'
+
+if (typeof window !== 'undefined') {
+  alert('🔍 NEXT_PUBLIC_SUPABASE_URL: ' + process.env.NEXT_PUBLIC_SUPABASE_URL);
+  alert('🔍 NEXT_PUBLIC_SUPABASE_ANON_KEY: ' + process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+}
 
 export const createClient = () => {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   )
-}
+} 
