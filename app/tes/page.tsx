432905b0a4aa95882744381688012e59fefdd9f2
samysediri <samysediri65@gmail.com>
2025-04-05 16:40:06 -0400

    Create page.tsx

---
+++ app/tes/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function TestPage() {
+  alert('✅ Page /test chargée côté client');
+  
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>Page de test</h1>
+    </div>
+  );
+}
