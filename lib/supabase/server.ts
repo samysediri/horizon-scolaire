6bad538f44fdeb3830295b52d3d2b076bf0c14cb
samysediri <samysediri65@gmail.com>
2025-04-05 20:16:07 -0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,14 +1,23 @@
 // lib/supabase/server.ts
-
 import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
+import { cookies as nextCookies } from 'next/headers'
 
 export const createClient = () => {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
-      cookies
+      cookies: {
+        get(name: string) {
+          return nextCookies().get(name)?.value
+        },
+        set(name: string, value: string, options: any) {
+          nextCookies().set({ name, value, ...options })
+        },
+        remove(name: string, options: any) {
+          nextCookies().delete({ name, ...options })
+        },
+      },
     }
   )
 }
