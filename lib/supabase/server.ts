2f06c3fba6bfc707e896b6af44daa96756a67c99
samysediri <samysediri65@gmail.com>
2025-04-05 20:14:26 -0400

    Create server.ts

---
+++ lib/supabase/server.ts
@@ -0,0 +1,14 @@
+// lib/supabase/server.ts
+
+import { createServerClient } from '@supabase/ssr'
+import { cookies } from 'next/headers'
+
+export const createClient = () => {
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies
+    }
+  )
+}
