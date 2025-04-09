835038bf6fda9045cfa65188b75eab40e186e6ff
samysediri <samysediri65@gmail.com>
2025-04-09 14:58:08 -0400

    Update page.tsx

--- app/creer-mot-de-passe/page.tsx
+++ app/creer-mot-de-passe/page.tsx
@@ -1,19 +1,5 @@
-// app/creer-mot-de-passe/page.tsx
-
-import { Suspense } from 'react'
 import CreatePasswordClient from './CreatePasswordClient'
 
-export const metadata = {
-  title: 'Créer un mot de passe',
-}
-
 export default function CreatePasswordPage() {
-  return (
-    <>
-      <h1>Créer un mot de passe</h1>
-      <Suspense fallback={<p>Chargement...</p>}>
-        <CreatePasswordClient />
-      </Suspense>
-    </>
-  )
+  return <CreatePasswordClient />
 }
