b8a64450750c731a7133f744a1fa427da2006e3a
samysediri <samysediri65@gmail.com>
2025-04-09 15:19:14 -0400

    Update page.tsx

--- app/creer-mot-de-passe/page.tsx
+++ app/creer-mot-de-passe/page.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from 'react'
 import CreatePasswordClient from './CreatePasswordClient'
 
 export default function CreatePasswordPage() {
-  return <CreatePasswordClient />
+  return (
+    <Suspense fallback={<div>Chargement...</div>}>
+      <CreatePasswordClient />
+    </Suspense>
+  )
 }
