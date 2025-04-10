f9f348ead27dee4d0fec89c410b037e6b734d640
samysediri <samysediri65@gmail.com>
2025-04-10 11:40:09 -0400

    Create provider.tsx

---
+++ lib/supabase/provider.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import { createPagesBrowserClient } from '@supabase/auth-helpers-nextjs'
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+
+export function SupabaseProvider({ children }: { children: React.ReactNode }) {
+  const [supabaseClient] = useState(() => createPagesBrowserClient())
+
+  return (
+    <SessionContextProvider supabaseClient={supabaseClient}>
+      {children}
+    </SessionContextProvider>
+  )
+}
