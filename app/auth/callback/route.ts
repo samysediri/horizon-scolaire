36b3ef2dfa84b415cbf6037850a89d93ff775ccb
samysediri <samysediri65@gmail.com>
2025-04-12 13:42:13 -0400

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from 'next/server'
 
 export async function GET(request: Request) {
   const cookieStore = cookies()
-  const supabase = createServerClient(cookieStore)
+  const supabase = createServerClient()
 
   const {
     data: { user },
