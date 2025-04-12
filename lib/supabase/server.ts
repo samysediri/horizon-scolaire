46caac8cb5ec097cb67e625f5309dbe1b16cdc4a
samysediri <samysediri65@gmail.com>
2025-04-12 15:30:17 -0400

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,13 @@
-// lib/supabase/server.ts
 import { cookies } from 'next/headers'
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createServerClient } from '@supabase/ssr'
 import type { Database } from '@/lib/database.types'
 
 export function createServerClient() {
-  return createServerComponentClient<Database>({ cookies })
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies
+    }
+  )
 }
