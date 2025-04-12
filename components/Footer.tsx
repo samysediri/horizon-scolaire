164eb2553e29bff3edd0c16767352d987be42d91
samysediri <samysediri65@gmail.com>
2025-04-12 12:44:46 -0400

    Delete components directory

--- components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="mt-10 p-4 border-t text-center text-sm text-gray-500">
-      © 2025 Horizon Scolaire. Tous droits réservés.
-    </footer>
-  )
-}
