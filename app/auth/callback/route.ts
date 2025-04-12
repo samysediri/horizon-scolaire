fbf6169f95ce61831ee15f8e36a5d7a71f9e6b05
samysediri <samysediri65@gmail.com>
2025-04-12 15:15:47 -0400

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,14 +1,16 @@
 // app/auth/callback/route.ts
 import { cookies } from 'next/headers'
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import type { Database } from '@/lib/database.types'
+import type { NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const cookieStore = cookies()
-  const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
+  const supabase = createRouteHandlerClient<Database>({
+    cookies
+  })
 
+  // ⚠️ ici on passe l'URL et non l'objet request au complet
   const { error } = await supabase.auth.exchangeCodeForSession(request.url)
 
   if (error) {
@@ -16,5 +18,6 @@ export async function GET(request: NextRequest) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
+  // Si tout va bien, redirige vers le dashboard (le redirect intelligent sera fait là-bas)
   return NextResponse.redirect(new URL('/dashboard', request.url))
 }
