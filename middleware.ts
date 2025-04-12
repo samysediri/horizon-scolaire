19d5a4d21d484e685b7b62a9f0d6593d7a812b0e
samysediri <samysediri65@gmail.com>
2025-04-12 15:01:15 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -2,14 +2,17 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
-import type { Database } from './lib/database.types'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
-  const supabase = createMiddlewareClient<Database>({ req, res })
+  const supabase = createMiddlewareClient({ req, res })
 
   // Ceci hydrate les cookies pour que le PKCE verifier soit disponible plus tard
   await supabase.auth.getSession()
 
   return res
+}
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico).*)'],
 }
