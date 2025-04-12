575bfb7d2dfa44a8fc434fe641f5cb7c3975cd55
samysediri <samysediri65@gmail.com>
2025-04-12 15:02:49 -0400

    Update route.ts

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,13 +1,15 @@
 // app/auth/callback/route.ts
 import { cookies } from 'next/headers'
-import { NextRequest, NextResponse } from 'next/server'
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import type { Database } from '@/lib/database.types'
 
 export async function GET(request: NextRequest) {
   const cookieStore = cookies()
-  const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
+  const supabase = createRouteHandlerClient<Database>({ cookies: () => cookieStore })
 
-  const { error } = await supabase.auth.exchangeCodeForSession(request)
+  const { error } = await supabase.auth.exchangeCodeForSession(request.url)
 
   if (error) {
     console.error("Erreur d'échange de session :", error.message)
