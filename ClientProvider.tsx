0afde4c9140c441cd37aa3bf7f2e5d2a2a4bfed0
samysediri <samysediri65@gmail.com>
2025-04-13 21:06:38 -0400

    Create ClientProvider.tsx

---
+++ ClientProvider.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useState } from 'react';
+import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+
+export default function ClientProvider({ children }: { children: React.ReactNode }) {
+  const [supabase] = useState(() => createBrowserSupabaseClient());
+
+  return (
+    <SessionContextProvider supabaseClient={supabase}>
+      {children}
+    </SessionContextProvider>
+  );
+}
