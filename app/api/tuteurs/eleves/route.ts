aadd7ce02a9d3765976dd2ac56ec6c8bd2f0a997
samysediri <samysediri65@gmail.com>
2025-04-16 11:38:49 -0400

    Update route.ts

--- app/api/tuteurs/eleves/route.ts
+++ app/api/tuteurs/eleves/route.ts
@@ -28,7 +28,15 @@ export async function GET() {
 
   const { data, error } = await supabase
     .from('tuteurs_eleves')
-    .select('eleves!tuteurs_eleves_eleve_id_fkey(id, prenom, nom, email)')
+    .select(`
+      eleves!tuteurs_eleves_eleve_id_fkey(
+        id,
+        prenom,
+        nom,
+        email,
+        lien_lessonspace
+      )
+    `)
     .eq('tuteur_id', tuteur_id)
 
   if (error) {
