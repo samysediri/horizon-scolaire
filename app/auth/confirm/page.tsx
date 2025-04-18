647b7461988a61740057cf2f9cc90e31500017ce
samysediri <samysediri65@gmail.com>
2025-04-18 16:58:31 -0400

    Update page.tsx

--- app/auth/confirm/page.tsx
+++ app/auth/confirm/page.tsx
@@ -8,23 +8,17 @@ export default function ConfirmPage() {
   const router = useRouter();
 
   useEffect(() => {
-    const hash = window.location.hash;
-    const params = new URLSearchParams(hash.substring(1));
-    const accessToken = params.get('access_token');
+    const supabase = createPagesBrowserClient();
 
-    if (accessToken) {
-      const supabase = createPagesBrowserClient();
-      supabase.auth
-        .getSessionFromUrl({ storeSession: true })
-        .then(() => {
-          router.push('/auth/confirm');
-        })
-        .catch(() => {
-          router.push('/login');
-        });
-    } else {
-      router.push('/login');
-    }
+    // Cette méthode s'occupe de récupérer access_token & refresh_token dans l'URL
+    supabase.auth
+      .exchangeCodeForSession()
+      .then(() => {
+        router.push('/auth/confirm');
+      })
+      .catch(() => {
+        router.push('/login');
+      });
   }, []);
 
   return (
